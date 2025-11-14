79886057eabb6d9af69e63e5d97293ea0b77bffe
waskylinks <likitajoel@gmail.com>
2025-11-14 22:35:36 +0100

    feat: connect to mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const connectDB = require('./Database/database');
+// Connect to the database
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`)
+})
