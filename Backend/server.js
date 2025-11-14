03cec685cf41c7102a24f57292d80a6d90c657c2
waskylinks <likitajoel@gmail.com>
2025-11-14 22:47:28 +0100

    fix: .env

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,11 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+// home route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Epic Store Backend!');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`)
