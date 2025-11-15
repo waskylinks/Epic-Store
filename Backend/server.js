7c936fbbbe7356e5f408c5e3f36bf532aa62b2f1
waskylinks <likitajoel@gmail.com>
2025-11-15 11:06:22 +0100

    feat: port linked with .env, nodemon

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,28 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+
+
+
+
+
+
+dotenv.config('config/config.env');
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`)
+})
+
+/*
 require('dotenv').config();
-const express = require('express');
-const app = express();
 
 const connectDB = require('./Database/database');
 // Connect to the database
 connectDB();
 
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
-// home route
+/ home route
 app.get('/', (req, res) => {
     res.send('Welcome to the Epic Store Backend!');
 });
-
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on PORT ${PORT}`)
-})
+    */
