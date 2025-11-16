4f3cdf0631a8558aca1137dc12411cbd46e9884b
waskylinks <likitajoel@gmail.com>
2025-11-16 13:29:51 +0100

    feat: handle cast error

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -1,6 +1,14 @@
+import HandleError from '../utils/handleError.js';
+
 export default (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
+
+    //cast error
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new HandleError(message, 400);
+    }
 
     res.status(err.statusCode).json({
         success: false,
