58189bf6d15103ec620e34f11f2fbcc66b4dd831
waskylinks <likitajoel@gmail.com>
2025-11-17 11:52:48 +0100

    feat: user model logic

---
+++ Backend/models/userModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Name cannot exceed 30 characters"],
+        minLength: [3, "Name should have more than 4 characters"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [8, "Password should be greater than 8 characters"],
+        select: false,
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        }
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    resetPasswordToken: {
+        type: String,
+    },
+    resetPasswordExpire: {
+        type: Date,
+    },
+    
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
