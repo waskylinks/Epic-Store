bb3b818bc6c31ea3a68821151866c451ff81bd6d
waskylinks <likitajoel@gmail.com>
2025-11-18 13:57:55 +0100

    feat: auth user login

--- Backend/middleware/user-auth.js
+++ Backend/middleware/user-auth.js
@@ -1,7 +1,19 @@
+import HandleError from '../utils/handleError.js';
 import handleAsyncError from './handleAsyncError.js'
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
 export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
-    const token = req.cookies;
-    console.log(token);
+    const {token} = req.cookies;
+
+    if(!token){
+        return next(new HandleError('Authentication is missing! Please log in to access resource', 401));
+    }
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
+   
+    req.user = await User.findById(decodedData.id);
+
+    next();
 
 });
--- Backend/routes/products-route.js
+++ Backend/routes/products-route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { getProductDetails, getAllProducts, updateProduct, createProducts, deleteProduct } from '../controller/product-controller.js';
+import { verifyUserAuth } from '../middleware/user-auth.js';
+
 
 const router = express.Router();
 
 router.route("/products")
-.get(getAllProducts)
+.get(verifyUserAuth, getAllProducts)
 .post(createProducts);
 
 router.route('/product/:id')
