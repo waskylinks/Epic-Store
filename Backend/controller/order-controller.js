8b5ebbc937b80d9febb85484d510abfc73450b82
waskylinks <likitajoel@gmail.com>
2025-11-22 15:41:18 +0100

    feat: get single order by admin logic

--- Backend/controller/order-controller.js
+++ Backend/controller/order-controller.js
@@ -3,6 +3,7 @@ import Product from '../models/product-model.js';
 import Errorhandler from '../utils/handleError.js';
 import User from '../models/userModel.js';
 import handleAsyncError from '../middleware/handleAsyncError.js';
+import HandleError from '../utils/handleError.js';
 
 //create new order
 export const createNewOrder = handleAsyncError(async (req, res, next) => {
@@ -26,3 +27,17 @@ export const createNewOrder = handleAsyncError(async (req, res, next) => {
     });
 
 });
+
+//getting single order
+export const getSingleOrder = handleAsyncError(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+    if(!order) {
+        return next(new HandleError('No order found', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+    
+})
--- Backend/routes/order-routes.js
+++ Backend/routes/order-routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import { roleBaseAccess, verifyUserAuth } from '../middleware/user-auth.js';
-import { createNewOrder } from '../controller/order-controller.js';
+import { createNewOrder, getSingleOrder } from '../controller/order-controller.js';
 
 const router = express.Router();
 
 router.route('/new/order/').post(verifyUserAuth, createNewOrder);
+
+router.route('/admin/order/:id').get(verifyUserAuth, roleBaseAccess('admin'), getSingleOrder);
 
 export default router;
