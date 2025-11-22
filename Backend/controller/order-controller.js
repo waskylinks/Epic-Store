2a3fde487256da97d5a14b87b51ac9874cf434ef
waskylinks <likitajoel@gmail.com>
2025-11-22 15:54:50 +0100

    feat: get all user orders logic

--- Backend/controller/order-controller.js
+++ Backend/controller/order-controller.js
@@ -39,5 +39,20 @@ export const getSingleOrder = handleAsyncError(async (req, res, next) => {
         success: true,
         order
     });
-    
+
+})
+
+//all orders 
+export const allMyOrders = handleAsyncError(async (req, res, next) => {
+    const orders = await Order.find({
+        user: req.user._id
+    });
+    if(!orders) {
+        return next(new HandleError('No order found', 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
 })
--- Backend/routes/order-routes.js
+++ Backend/routes/order-routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { roleBaseAccess, verifyUserAuth } from '../middleware/user-auth.js';
-import { createNewOrder, getSingleOrder } from '../controller/order-controller.js';
+import { allMyOrders, createNewOrder, getSingleOrder } from '../controller/order-controller.js';
 
 const router = express.Router();
 
 router.route('/new/order/').post(verifyUserAuth, createNewOrder);
 
 router.route('/admin/order/:id').get(verifyUserAuth, roleBaseAccess('admin'), getSingleOrder);
+
+router.route('/orders/user/').get(verifyUserAuth, allMyOrders);
 
 export default router;
