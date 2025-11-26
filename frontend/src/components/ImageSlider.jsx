1d876ebc9328fb895a8f49851048d6859725d2a3
waskylinks <likitajoel@gmail.com>
2025-11-26 12:49:57 +0100

    feat: image slider logic

---
+++ frontend/src/components/ImageSlider.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import '../componentStyles/ImageSlider.css'
+
+const images = [
+    "./images/blackfriday1.webp",
+    "./images/blackfriday2.webp",
+    "./images/blackfriday3.webp",
+    "./images/blackfriday4.webp",
+]
+
+function ImageSlider() {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex+1) % images.length)
+
+        }, 5000)
+        return () => clearInterval(interval);
+    }, [])
+
+  return (
+    <div className="image-slider-container">
+        <div className="slider-images" style={{transform: `translateX(-${currentIndex * 100}%)`}}>
+
+            { images.map((image, index) => (
+                <div className="slider-item" key={index}>
+                <img src={image} alt={`Slide ${index+1}`} />
+            </div>
+            ))
+}
+        </div>
+
+        <div className="slider-dots">
+                {images.map((_, index) => (
+                    <span className={`dot ${index === currentIndex ? 'active' : ''}`} onClick={() => setCurrentIndex(index)}/>
+                ))}
+            </div>
+    </div>
+  )
+}
+
+export default ImageSlider
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../components/footer';
 import '../pageStyles/Home.css'
 import Navbar from '../components/Navbar';
+import ImageSlider from '../components/ImageSlider';
 
 function Home() {
     return (
         <>
         <Navbar/>
+        <ImageSlider/>
         <div className="home-container">
             <h2 className="home-heading">
                 Trending Now
