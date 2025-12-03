eb0320ef6222107a258612af15a0b9b089546882
waskylinks <likitajoel@gmail.com>
2025-12-03 20:59:36 +0100

    feat: update user profile logic

--- frontend/src/User/UpdateProfile.jsx
+++ frontend/src/User/UpdateProfile.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import '../UserStyles/Form.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
+import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { removeErrors, removeSuccess, updateProfile } from '../features/products/userSlice';
+import Loader from '../components/Loader';
 
 function UpdateProfile() {
     const [name, setName] = useState('');
@@ -9,38 +14,112 @@ function UpdateProfile() {
     const [avatar, setAvatar] = useState('');
     const [avatarPreview, setAvatarPreview] = useState('./images/profile.webp');
 
+    const {user, error, success, message, loading} = useSelector((state) => state.user)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const profileImageUpdate = (e) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                if(reader.readyState === 2) {
+                    setAvatarPreview(reader.result)
+                    setAvatar(reader.result)
+                }
+            }
+            reader.onerror = () => {
+                toast.error('Error reading file:')
+            }
+            reader.readAsDataURL(e.target.files[0]);
+    }
+
+    const updateSubmit = (e) => {
+        e.preventDefault();
+        const myForm = new FormData();
+        myForm.set('name', name)
+        myForm.set('email', email)
+        myForm.set('avatar', avatar)
+        dispatch(updateProfile(myForm))
+    }
+
+    useEffect(() => {
+            if(error) {
+                toast.error(error, {position: 'top-center', autoClose: 2000});
+                dispatch(removeErrors())
+            }
+        }, [dispatch, error])
+
+    useEffect(() => {
+            if(success) {
+                toast.success(message, {position: 'top-center', autoClose: 2000});
+                dispatch(removeSuccess())
+                navigate('/profile')
+            }
+        }, [dispatch, success, message, navigate])
+
+    useLayoutEffect(() => {
+        if(user) {
+            setName(user.name)
+            setEmail(user.email)
+            setAvatarPreview(user?.avatar?.url || './images/profile.webp')
+        }
+    }, [user])
+
   return (
     <>
+    { loading? (<Loader />) : (<>
     <Navbar />
 
     <div className="container update-container">
         <div className="form-content">
-            <form  className="form">
+            <form  
+            className="form" 
+            encType='multipart/form-data' 
+            onSubmit={updateSubmit} >
+
                 <h2>
                     Update Profile
                 </h2>
 
                 <div className="input-group avatar-group">
-                    <input type="file" className="file-input" accept='image'/>
-                    <img src={avatarPreview} alt="User profile" className="avatar" />
+                    <input 
+                    type="file" 
+                    className="file-input" 
+                    accept="image/*" 
+                    onChange={profileImageUpdate}
+                    name='avatar'/>
+
+                    <img 
+                    src={avatarPreview} 
+                    alt="User profile" 
+                    className="avatar" />
                 </div>
 
                 <div className="input-group">
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input 
+                    type="text" 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}
+                    name='name'/>
                 </div>
 
                 <div className="input-group">
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                    name='email'/>
                 </div>
 
                 <button className="authBtn">
                     Update
                 </button>
+
             </form>
         </div>
     </div>
 
     <Footer />
+    </>)}
     </>
   )
 }
--- frontend/src/features/products/userSlice.js
+++ frontend/src/features/products/userSlice.js
@@ -38,7 +38,7 @@ export const login = createAsyncThunk('user/login', async({email, password}, {re
 //load user
 export const loadUser = createAsyncThunk('user/loadUser', async(_, {rejectWithValue}) => {
     try{
-        const {data} = axios.get('/api/v1/profile');
+        const {data} = await axios.get('/api/v1/profile');
         return data
 
     } catch (error) {
@@ -49,7 +49,7 @@ export const loadUser = createAsyncThunk('user/loadUser', async(_, {rejectWithVa
 //logout user
 export const logout = createAsyncThunk('user/logout', async(_, {rejectWithValue}) => {
     try{
-        const {data} = axios.post('/api/v1/logout', {
+        const {data} = await axios.post('/api/v1/logout', {
         withCredentials : true
         });
 
@@ -60,6 +60,24 @@ export const logout = createAsyncThunk('user/logout', async(_, {rejectWithValue}
     }
 })
 
+
+//update profile
+export const updateProfile = createAsyncThunk('user/updateProfile', async(userData, {rejectWithValue}) => {
+    try{
+        const config = {
+            headers : {
+                'Content-Type' : 'multipart/form-data'
+            }
+        }
+        const {data} = await axios.put('/api/v1/profile/update', userData, config);
+
+        return data
+
+    } catch (error) {
+        return rejectWithValue(error.response?.data || {message: `Profile update failed. Please try again later`});
+    }
+})
+
 const userSlice = createSlice({
     name: 'user',
     initialState: {
@@ -67,7 +85,8 @@ const userSlice = createSlice({
         loading: false,
         error: null,
         success: false,
-        isAuthenticated: false
+        isAuthenticated: false,
+        message: null
     },
     reducers: {
         removeErrors: (state) => {
@@ -149,6 +168,23 @@ const userSlice = createSlice({
             .addCase(logout.rejected, (state, action) => {
                 state.loading = false;
                 state.error = action.payload?.message || 'Failed to logout. Please try again later'
+            })
+
+            //update user profile
+            builder.addCase(updateProfile.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(updateProfile.fulfilled, (state, action) => {
+                state.loading = false;
+                state.error = null;
+                state.user = action.payload?.user || null
+                state.success = action.payload?.success
+                state.message = action.payload?.message
+            })
+            .addCase(updateProfile.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload?.message || 'Profile update failed. Please try again later'
             })
     }
     
