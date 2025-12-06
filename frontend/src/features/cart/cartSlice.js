dd844bc17b5f6c1657dec5a6f7d2ea7d15f3d67f
waskylinks <likitajoel@gmail.com>
2025-12-06 11:03:23 +0100

    feat: save product added to cart in local storage

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -23,7 +23,7 @@ export const addItemsToCart = createAsyncThunk('user/addItemsToCart', async({id,
 const cartSlice = createSlice ({
     name : 'cart',
     initialState : {
-        cartItems : [],
+        cartItems : JSON.parse(localStorage.getItem('cartItems')) || [],
         loading : false,
         error : null,
         success : false,
@@ -48,10 +48,23 @@ const cartSlice = createSlice ({
         builder.addCase(addItemsToCart.fulfilled, (state, action) => {
             const item = action.payload
             state.cartItems.push(item)
+
+            const existingItem = state.cartItems.find(
+                (i) => i.product === item.product)
+
+            if(existingItem) {
+                existingItem.quantity = item.quantity
+                state.message = `Updated ${item.name} quantity in the cart`
+            } else {
+                state.cartItems.push(item)
+                state.message = `${item.name} is added to cart successfully`
+            }
+
             state.loading = false;
             state.error = null,
             state.success = true
-            state.message = `${item.name} is added to cart successfully`
+            localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
+            
         })
 
         builder.addCase(addItemsToCart.rejected, (state, action) => {
