150567d7a91c6f4b36674804bd250641e8212c29
waskylinks <likitajoel@gmail.com>
2025-12-24 21:40:15 +0100

    feat: admin view available products

--- frontend/src/Admin/ProductList.jsx
+++ frontend/src/Admin/ProductList.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PageTitle from '../components/PageTitle'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
 import '../AdminStyles/ProductsList.css'
 import { Link } from 'react-router-dom'
 import { Delete, Edit } from '@mui/icons-material'
+import { useDispatch, useSelector } from 'react-redux'
+import {fetchAdminProducts, removeErrors} from '../features/admin/adminSlice'
+import { toast } from 'react-toastify'
+import Loader from '../components/Loader'
 
 function ProductList() {
+    const {products, loading, error} = useSelector(state => state.admin);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchAdminProducts())
+    }, [dispatch]);
+
+    useEffect(() => {
+        if(error) {
+            toast.error(error, { position: 'top-center', autoClose: 2000 })
+            dispatch(removeErrors());
+        }
+    }, [dispatch, error])
+
+    if(!products || products.length === 0) {
+        return (
+            <div className="product-list-container">
+                <h1 className="product-list-title">
+                    Admin Products
+                </h1>
+                <p className="no-admin-product">
+                    No Products Found
+                </p>
+            </div>
+        )
+    }
+
   return (
     <>
+    { loading ? (<Loader />) :
+        (<>
     <PageTitle title='Admin Products'/>
     <Navbar />
 
@@ -31,45 +63,54 @@ function ProductList() {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>
-                        <img src="" alt="" />
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        {}
-                    </td>
-                    <td>
-                        <Link to='/admin/product/:productId'>
-                            <Edit />
-                        </Link>
-                        <Link to='/admin/product/:productId'>
-                            <Delete />
-                        </Link>
-                    </td>
-                </tr>
+                { products.map((product, index) => {
+                    <tr key={product._id}>
+                        <td>
+                            {index+1}
+                        </td>
+                        <td>
+                            <img src={product.image[0].url} alt={product.name}  className='admin-product-image'/>
+                        </td>
+                        <td>
+                            {product.name}
+                        </td>
+                        <td>
+                            {product.price}
+                        </td>
+                        <td>
+                            {product.ratings}
+                        </td>
+                        <td>
+                            {product.category}
+                        </td>
+                        <td>
+                            {product.stock}
+                        </td>
+                        <td>
+                            {new Date(product.createdAt).toLocaleString()}
+                        </td>
+                        <td>
+                            <Link 
+                            to={`/admin/product/${product._id}`}
+                            className='action-icon-edit'>
+                                <Edit />
+                            </Link>
+                            <Link to={`/admin/product/${product._id}`}
+                            className='action-icon-delete'>
+                                <Delete />
+                            </Link>
+                        </td>
+                    </tr>
+                })}
             </tbody>
         </table>
     </div>
 
     <Footer />
+    </>)}
     </>
   )
 }
+
 
 export default ProductList
