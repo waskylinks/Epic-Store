e9065d2a0b83f2aa1afd0b075ffa5bab13797a5e
waskylinks <likitajoel@gmail.com>
2025-12-26 20:10:19 +0100

    feat: create product image with cloudinary

--- Backend/controller/product-controller.js
+++ Backend/controller/product-controller.js
@@ -2,6 +2,7 @@ import Product from '../models/product-model.js';
 import HandleError from '../utils/handleError.js';
 import handleAsyncError from '../middleware/handleAsyncError.js';
 import APIFunctionality from '../utils/apiFunctionality.js';
+import {v2 as cloudinary} from 'cloudinary';
 
 //http://localhost:8000/api/v1/product/69189630f8a419d4bf0dd35a?keyword=shirt
 
@@ -9,6 +10,27 @@ import APIFunctionality from '../utils/apiFunctionality.js';
 
 // creating products 
 export const createProducts = handleAsyncError(async (req, res, next) => {
+    let image = [];
+    if(typeof req.body.image === 'string'){
+        image.push(req.body.image);
+    }else{
+        image = req.body.image;
+    }
+
+    const imageLinks = [];
+
+    for(let i = 0; i < image.length; i++){
+        const result = await cloudinary.uploader.upload(image[i], {
+            folder: 'products'
+        });
+        imageLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        })
+    }
+
+    req.body.image = imageLinks;
+
     req.body.user = req.user.id;
 
     const product = await Product.create(req.body)
