203a2ae992c2b3bef8797ea728585835651bdae0
waskylinks <likitajoel@gmail.com>
2026-01-03 18:22:05 +0100

    fix: createdAt to reviews modal

--- Backend/models/product-model.js
+++ Backend/models/product-model.js
@@ -64,6 +64,10 @@ const productSchema = new mongoose.Schema({
             comment: {
                 type: String,
                 required: true,
+            },
+            createdAt: {
+            type: Date,
+            default: Date.now,
             }
         }
     ],
