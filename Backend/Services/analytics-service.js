5a32f135fa873c82ed8a8d6d2e90d33a0a248afb
waskylinks <likitajoel@gmail.com>
2026-01-03 21:18:47 +0100

    fix: analytic services

--- Backend/Services/analytics-service.js
+++ Backend/Services/analytics-service.js
@@ -1,65 +1,22 @@
-import { ORDER_STATUSES } from "../constants/analytics.constants.js";
-import Order from "../models/order-model.js";
 import Product from "../models/product-model.js";
+import Order from "../models/order-model.js";
 import User from "../models/user-model.js";
-
+import { assertAggregationResult } from "../utils/analyticsGuard.js";
+import { orderStatsPipeline, productStatsPipeline, userStatsPipeline } from "../constants/analytics-pipelines.js";
 
 export const getAdminStatsService = async () => {
-  const [products, orders, users] = await Promise.all([
-    Product.aggregate([
-      {
-        $group: {
-          _id: null,
-          products: { $sum: 1 },
-          outOfStock: {
-            $sum: { $cond: [{ $eq: ["$stock", 0] }, 1, 0] }
-          }
-        }
-      },
-      {
-        $project: {
-          _id: 0,
-          products: 1,
-          outOfStock: 1,
-          inStock: { $subtract: ["$products", "$outOfStock"] }
-        }
-      }
-    ]),
+  const productStats = await Product.aggregate(productStatsPipeline);
+  assertAggregationResult(productStats, "Product stats aggregation");
 
-    Order.aggregate([
-      {
-        $group: {
-          _id: null,
-          orders: { $sum: 1 },
-          revenue: {
-            $sum: {
-              $cond: [
-                { $ne: ["$orderStatus", ORDER_STATUSES.CANCELLED] },
-                "$totalPrice",
-                0
-              ]
-            }
-          }
-        }
-      }
-    ]),
+  const orderStats = await Order.aggregate(orderStatsPipeline);
+  assertAggregationResult(orderStats, "Order stats aggregation");
 
-    User.aggregate([
-      {
-        $group: {
-          _id: null,
-          users: { $sum: 1 },
-          adminCount: {
-            $sum: { $cond: [{ $eq: ["$role", "admin"] }, 1, 0] }
-          }
-        }
-      }
-    ])
-  ]);
+  const userStats = await User.aggregate(userStatsPipeline);
+  assertAggregationResult(userStats, "User stats aggregation");
 
   return {
-    products: products[0] || {},
-    orders: orders[0] || {},
-    users: users[0] || {}
+    products: productStats[0],
+    orders: orderStats[0],
+    users: userStats[0]
   };
 };
