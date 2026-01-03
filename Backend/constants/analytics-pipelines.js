3229a9544bd3005c3f5fea3231ceb5ac5518df46
waskylinks <likitajoel@gmail.com>
2026-01-03 20:38:59 +0100

    feat: analytics pipeline

---
+++ Backend/constants/analytics-pipelines.js
@@ -0,0 +1,49 @@
+export const productStatsPipeline = [
+  {
+    $group: {
+      _id: null,
+      products: { $sum: 1 },
+      outOfStock: {
+        $sum: { $cond: [{ $eq: ["$stock", 0] }, 1, 0] }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      products: 1,
+      outOfStock: 1,
+      inStock: { $subtract: ["$products", "$outOfStock"] }
+    }
+  }
+];
+
+export const orderStatsPipeline = [
+  {
+    $group: {
+      _id: null,
+      orders: { $sum: 1 },
+      revenue: {
+        $sum: {
+          $cond: [
+            { $ne: ["$orderStatus", "cancelled"] },
+            "$totalPrice",
+            0
+          ]
+        }
+      }
+    }
+  }
+];
+
+export const userStatsPipeline = [
+  {
+    $group: {
+      _id: null,
+      users: { $sum: 1 },
+      adminCount: {
+        $sum: { $cond: [{ $eq: ["$role", "admin"] }, 1, 0] }
+      }
+    }
+  }
+];
